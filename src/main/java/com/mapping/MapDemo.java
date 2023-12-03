f6394bee1a5e4758a758572b4858f8a5b90da736
shakti <shaktisarswat01@gmail.com>
2023-12-03 18:45:06 +0530

    OneToOne Mapping explored

---
+++ src/main/java/com/mapping/MapDemo.java
@@ -0,0 +1,24 @@
+package com.mapping;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class MapDemo {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        Session session = factory.openSession();
+        session.beginTransaction();
+
+        // creating Question object
+
+        Answer answer = new Answer(1, "Java is a programming language");
+        Question question = new Question(1, "What is the Java", answer);
+
+        session.save(answer);
+        session.save(question);
+        session.getTransaction().commit();
+        session.close();
+        factory.close();
+    }
+}
